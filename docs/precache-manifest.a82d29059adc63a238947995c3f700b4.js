cdff47115c21ec69bd312d68af1667be991c89d2
cgonzaru <cristina.glez.rubio@gmail.com>
2019-12-20 15:00:23 +0100

    generate docs

---
+++ docs/precache-manifest.a82d29059adc63a238947995c3f700b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3187afc10763cce65f744cdad8a1ab4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4bd1598e97120e09a64e",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8cec76531be3cdd3843f",
+    "url": "./static/js/2.d1acde0b.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "./static/js/2.d1acde0b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4bd1598e97120e09a64e",
+    "url": "./static/js/main.1762901d.chunk.js"
+  },
+  {
+    "revision": "0505424efd4d6b4068d5",
+    "url": "./static/js/runtime-main.a2652788.js"
+  },
+  {
+    "revision": "12e6ea940b5d05692fd257dec6f3a622",
+    "url": "./static/media/banana.12e6ea94.jpeg"
+  }
+]);
